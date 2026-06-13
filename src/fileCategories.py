9100530a8193ae6258de1fd071e7aab7a179f165
Veronica <veronica.marchiori@estudante.uffs.edu.br>
2026-06-13 17:54:56 -0300

    feat: adicionar mapping de categorias de extensão de arquivos.

---
+++ src/fileCategories.py
@@ -0,0 +1,58 @@
+FILE_CATEGORIES = {
+
+    "Documents": [
+        "pdf",
+        "doc",
+        "docx",
+        "txt",
+        "xlsx",
+        "ppt",
+        "pptx",
+    ],
+
+    "Images": [
+        "png",
+        "jpg",
+        "jpeg",
+        "gif",
+        "webp",
+        "svg"
+    ],
+
+    "Design Projects": [
+        "sai",
+        "psd",
+        "kra",
+        "clip",
+        "xcf"
+    ],
+
+    "3DProjects":[
+        "blend"
+    ],
+
+    "Executables": [
+        "exe",
+        "msi"
+    ],
+
+    "Compressed": [
+        "zip",
+        "rar",
+        "7z"
+    ],
+
+    "Audio": [
+        "mp3",
+        "wav",
+        "ogg"
+    ],
+
+    "Videos": [
+        "mp4",
+        "avi",
+        "mkv",
+        "mov"
+    ]
+    
+}

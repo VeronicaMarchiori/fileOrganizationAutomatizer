68eb4344de8ecdd5f7b8e50a2f25a4b1023baa8e
Veronica <veronica.marchiori@estudante.uffs.edu.br>
2026-06-24 21:01:33 -0300

    feat: Adição de UI console para melhorar ux

---
+++ src/consoleUI.py
@@ -0,0 +1,27 @@
+class ConsoleUI:
+    def showHeader(self):
+        print("=" * 50)
+        print("     File Organization Automatizer")
+        print("=" * 50)
+
+    def showDirectory(self, directory):
+        print(f"\nDiretório analisado:\n{directory}")
+    
+    def showFilesPreview(self, files):
+        print (f"\nArquivos Encontrados: {len(files)}")
+
+        for file in files:
+            print(f"- {file.name}")
+
+    def askConfirmation(self) -> bool:
+        answer = input("\nDeseja organizar estes arquivos? [y/n]: ")
+        return answer.lower() == "y"
+    
+    def showCanceledMessage(self):
+        print("\nOperação cancelada.")
+    
+    def showSuccessMessage(self):
+        print("\nOrganização concluída com sucesso!")
+    
+    def waitBeforeClose(self):
+        input("\nPressione ENTER para fechar...")
--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
 from fileOrganizer import FileOrganizer
+from consoleUI import ConsoleUI
 
 def main ():
     organizer = FileOrganizer()
-    organizer.showFiles()
+    console = ConsoleUI()
+
+    files = organizer.listFiles()
+
+    console.showHeader()
+    console.showDirectory(organizer.currentDirectory)
+    console.showFilesPreview(files)
+
+    if not console.askConfirmation():
+        console.showCanceledMessage()
+        console.waitBeforeClose()
+        return
+    
     organizer.organizeFiles()
+
+    console.showSuccessMessage()
+    console.waitBeforeClose()
 
 if __name__ == "__main__":
     main()
